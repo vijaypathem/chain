dbeb1eb4189099cca2250897841848ee4f8252cf
vijay pathem <vijaypathem4@gmail.com>
2024-03-11 13:05:19 +0530

    css

--- Chain/settings.py
+++ Chain/settings.py
@@ -142,4 +142,11 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
